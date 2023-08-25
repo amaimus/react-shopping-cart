62323009d3ea1e36b0ea81f5a799121b9fc89597
Alejandro M <alejandromaimus147@gmail.com>
2023-08-26 00:18:49 +0200

    using useContext to avoid prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,19 @@ import { Products } from './components/Products'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import { products as initialProducts } from '../src/mocks/products.json'
-import { useState } from 'react'
 import { IS_DEVELOPMENT } from '../config.js'
-
-function useFilters () {
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice &&
-        (
-          filters.category === 'all' ||
-          product.category === filters.category
-        )
-      )
-    })
-  }
-  return { filters, filterProducts, setFilters }
-}
+import { useFilters } from './hooks/useFilters'
 
 function App () {
-  const [products] = useState(initialProducts)
-  const { filters, filterProducts, setFilters } = useFilters()
+  const { filterProducts } = useFilters()
 
-  const filteredProducts = filterProducts(products)
+  const filteredProducts = filterProducts(initialProducts)
 
   return (
     <>
-      <Header changeFilters={setFilters} />
+      <Header />
       <Products products={filteredProducts} />
-      {IS_DEVELOPMENT && <Footer filters={filters}/>}
+      {IS_DEVELOPMENT && <Footer />}
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
+import { useFilters } from '../hooks/useFilters'
 import './Footer.css'
 
-export function Footer ({ filters }) {
-  // const { filters } = useFilters()
+export function Footer () {
+  const { filters } = useFilters()
 
   return (
     <footer className='footer'>
-      { JSON.stringify(filters)}
+      { JSON.stringify(filters) }
     </footer>
   )
 }
