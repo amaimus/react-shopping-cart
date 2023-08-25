0174a1c4ff2424a9e32c991bf1bb3faa5137c217
Alejandro M <alejandromaimus147@gmail.com>
2023-08-25 18:17:48 +0200

    show products list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import { Products } from './components/Products'
+import { products } from '../src/mocks/products.json'
 
 function App () {
   return (
     <>
       <h1>Shopping cart 🛒</h1>
+      <Products products={products} />
     </>
   )
 }
