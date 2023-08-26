591fd8950124b8c05888210a8345ecd92393b9d5
Alejandro M <alejandromaimus147@gmail.com>
2023-08-26 11:11:59 +0200

    conditional rendering & remove from cart fnc

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -27,8 +27,12 @@ export function CartProvider ({ children }) {
     setCart([])
   }
 
+  const removeFromCart = product => {
+    setCart(prevState => prevState.filter(item => item.id !== product.id))
+  }
+
   return (
-    <CartContext.Provider value={{ cart, addToCart, clearCart }}>
+    <CartContext.Provider value={{ cart, addToCart, clearCart, removeFromCart }}>
       {children}
     </CartContext.Provider>
   )
